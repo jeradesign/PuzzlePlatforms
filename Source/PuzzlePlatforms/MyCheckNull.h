69c610e4ac4e6f3efb906f60e3948689cb0c0542
John Brewer <jbrewer@jera.com>
2021-12-09 12:56:35 -0800

    Add 2-arg MYCHECKNULL2 macro

--- Source/PuzzlePlatforms/MyCheckNull.h
+++ Source/PuzzlePlatforms/MyCheckNull.h
@@ -1,5 +1,9 @@
 #pragma once
-#define MYCHECKNULL(__pointer_to_check__) do { if (__pointer_to_check__ == nullptr) { \
+
+#define MYCHECKNULL2(__pointer_to_check__, statement)\
+do { if (__pointer_to_check__ == nullptr) { \
 	UE_LOG(LogTemp, Error, TEXT("null pointer: " #__pointer_to_check__));\
-	return;\
+	statement;\
 	} } while (false)
+
+#define MYCHECKNULL(__pointer_to_check__) MYCHECKNULL2(__pointer_to_check__, return)
